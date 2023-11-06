23aa4014697b84495bb97116862a9f492b458de0
Alex Sifuna <aleksifuna@gmail.com>
2023-11-06 23:50:19 +0000

    made some corrections on initialization method

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
                 elif key == "updated_at":
                     self.updated_at = datetime.fromisoformat(value)
                 else:
-                    self.key = value
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
