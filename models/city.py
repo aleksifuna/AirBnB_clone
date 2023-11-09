dac9ba0ba3ade252e6b5f5ca6e84e518b40381e7
Alex Sifuna <aleksifuna@gmail.com>
2023-11-09 00:12:39 +0000

    added city and place class

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""
+city class representation module
+"""
+
+
+class User(BaseModel):
+    """
+    Definition of class city that inherits from BaseModel
+    """
+
+    state_id = ""
+    name = ""
